91650ce58b0bb1834a10dcd80d3df00178dd9363
Rees Morris <gk57@rees.email>
2024-11-14 01:30:54 +0000

    Fix lint issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <p>
           Wow, you&apos;re here early! This site is still being developed.
           Please come back in like.. a day or two. The game barely even has
-          dailies yet! I'll be open-sourcing this project soon, so you can ask
-          me questions or contribute to the project on GitHub.
+          dailies yet! I&apos;ll be open-sourcing this project soon, so you can
+          ask me questions or contribute to the project on GitHub.
         </p>
       </Wrapper>
     </>
